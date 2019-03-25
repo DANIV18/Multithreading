3aac11f7b709b4cc61c97bed243521c511e9f2ff
DANIV18 <igordaniv242@gmail.com>
2019-03-25 11:05:20 +0200

    Was modified firstApp

--- simpleFirstApp/main.cpp
+++ simpleFirstApp/main.cpp
@@ -9,6 +9,21 @@ int main()
     {
         std::cout<<first<<" "<<second<<std::endl;
     };
-    std::thread th(tempFunc,"Hello","world");
-    th.join();
+
+    auto temp10Second = []()
+    {
+        for(size_t i(0); i != 10; ++i)
+        {
+            std::cout<<"temp10Second : "<<i<<" second"<<std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    };
+
+    std::thread th(temp10Second);
+    for(size_t i(0); i != 5; ++i)
+    {
+        std::cout<<"main : "<<i<<" second"<<std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
 }
